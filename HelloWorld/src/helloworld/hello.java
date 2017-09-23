626501bffd676b285c60f9da1f3be9b55123081b
xujiarong <1018769441@qq.com>
2017-09-23 15:33:56 +0800

    文件夹HelloWorld提交

---
+++ HelloWorld/src/helloworld/hello.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class hello {
+     private String s;
+     public String fuzhi(String str) {
+    	 s=str;
+    	 return s;
+     }
+     public void show() {
+    	 System.out.println(s);
+     }
+}
---
+++ HelloWorld/src/helloworld/main1.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class main1 {
+    public static void main(String [] args) {
+    	hello a=new hello();
+    	a.fuzhi("helloworld !!!");
+    	a.show();
+    }
+}
